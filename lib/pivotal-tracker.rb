9493158e044193dacad521492939a5d9e7e4182e
David Bayendor <dbayendor@gmail.com>
2013-09-07 13:33:42 -0600

    Add environment file to handle credentials

--- lib/pivotal-tracker.rb
+++ lib/pivotal-tracker.rb
@@ -1,17 +1,17 @@
 require 'version'
 require 'httparty'
 
-# class PivotalTracker
+class PivotalTracker
 
-# end
+end
 
 require 'pivotal-tracker/client'
 require 'pivotal-tracker/story'
 require 'pivotal-tracker/project'
 require 'pivotal-tracker/membership'
 
-# class PivotalTracker
-#   include HTTParty
+class PivotalTracker
+  include HTTParty
 
   base_uri = 'https://www.pivotaltracker.com/services/v5'
-# end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'vcr'
 require 'pivotal-tracker'
-TOKEN = ENV["PIVOTAL_TOKEN_ID"]
+TOKEN = ENV["PIVOTAL_TOKEN_ID"]  # here until we a better place for it
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
