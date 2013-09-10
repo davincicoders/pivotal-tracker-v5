3f7cb0c90416371dacbddaba4c081cfe2c5b95eb
David Bayendor <dbayendor@gmail.com>
2013-09-10 12:47:47 -0600

    Update pivotal-tracker.rb
    * Comment to include config.yml code in this file
    for later use.

--- lib/pivotal-tracker.rb
+++ lib/pivotal-tracker.rb
@@ -11,7 +11,11 @@ require 'pivotal-tracker/project'
 require 'pivotal-tracker/membership'
 
 class PivotalTracker
+  # logic suggests we will need config file accessed here
+  # config = YAML::load_file(File.dirname(__FILE__) + '/config/pivotal_credentials.yml')
   include HTTParty
 
   base_uri = 'https://www.pivotaltracker.com/services/v5'
+
+
 end
